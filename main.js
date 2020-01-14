a3f2d6995af82fc420efd8770ceab62115fdd716
MagiCarpy <49000672+MagiCarpy@users.noreply.github.com>
2020-01-14 15:57:05 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "100px";
-document.getElementsByTagName("p")[0].style.fontSize = "30px";
+document.getElementsByTagName("p")[0].style.fontSize = "40px";
